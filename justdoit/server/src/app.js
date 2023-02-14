e2d4bd1b1b1215d1e70de9f1fa14c73414c3aa7a
pgm-jefdenie <jefdeniel@icloud.com>
2023-02-14 10:34:59 +0100

    add public folder

--- justdoit/server/src/app.js
+++ justdoit/server/src/app.js
@@ -1,3 +1,27 @@
-console.log("just do it ");
+// 1. Import statements
+import express from "express";
+// om vanuit views de index.html toe te voegen, je hebt het pad nodig voor je homepage te definen. Je moet werken met absolute paden dus dit is de oplossing
+import path from "path";
 
-// ctrl c to stop the server
+// 2. create express app (eerst port declareren, dan app, dan pas de functies)
+const app = express();
+
+// 3. Define a route handler for the default home page
+const port = 3000;
+
+// GET method route
+app.get("/", (req, res) => {
+  const indexHTML = path.resolve("src", "views", "index.html");
+  console.log(indexHTML);
+  res.sendFile(indexHTML);
+});
+
+// POST method route
+app.post("/", (req, res) => {
+  res.send("POST request to the homepage, app runs just fine");
+});
+
+// 4. Start the app
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
