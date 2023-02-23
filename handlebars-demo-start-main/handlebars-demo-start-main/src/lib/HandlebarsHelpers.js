d7105dfa1ba9168a72069c55290da5ffde732627
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2023-02-23 10:56:53 +0100

    add handlebar button

--- handlebars-demo-start-main/handlebars-demo-start-main/src/lib/HandlebarsHelpers.js
+++ handlebars-demo-start-main/handlebars-demo-start-main/src/lib/HandlebarsHelpers.js
@@ -5,5 +5,20 @@
 import Handlebars from "handlebars"; // import the handlebars library
 
 export default {
-  em: () => `<em>Test</em>`,
+  em: (options) => `<em>${options.fn()}</em>`,
+
+  bold: (options) => `<strong>${options.fn({ firstname: "Tim" })}</strong>`,
+
+  link: (text, url, target) =>
+    `<a href="${url}" target="${target}">${text}</a>`,
+
+  button: (use, options) => {
+    const validUse = ["primary", "secondary", "success", "danger", "warning"];
+
+    if (validUse.includes(use)) {
+      return `<button class="${use}" type="button">${options.fn()}</button>`;
+    } else {
+      return `<button type="button">${options.fn()}</button>`;
+    }
+  },
 };
